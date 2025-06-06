14fe44677fe329efd248e68a209fbb4accf16ece
Adrian Vladu <avladu@cloudbasesolutions.com>
2025-06-06 10:58:05 +0300

    use windows-latest

--- BuildAutomation/BuildCloudbaseInitSetup.ps1
+++ BuildAutomation/BuildCloudbaseInitSetup.ps1
@@ -29,7 +29,7 @@ if ($platform -eq "x86") {
     $platformVCVarsRequired = "x86"
 }
 
-SetVCVars "2019" $platformVCVarsRequired
+SetVCVars "2022" $platformVCVarsRequired
 
 # Use v140 with GitHub workflows env
 ReplaceVSToolSet $VSPlatformToolSet
